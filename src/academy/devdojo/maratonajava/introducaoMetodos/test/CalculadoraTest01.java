3df1fc6bed9a763696ac4587c71e90e66b137154
Lucas Pina <ti.lucaspina@gmail.com>
2022-08-11 22:10:18 -0300

    inicio de metodos

---
+++ src/academy/devdojo/maratonajava/introducaoMetodos/test/CalculadoraTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducaoMetodos.test;
+
+import academy.devdojo.maratonajava.introducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
